a6d974aeb9ff7af512acf76f60bb1555d9308fb4
Ali-2544 <codewithpassion4@gmail.com>
2024-03-22 02:34:36 +0500

    22 march morning by Torretp@2544

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import "../../strapup/app/globals.css";
-<<<<<<< HEAD
-import Homesrc from "@/component/atoms/Homesrc";
-import Homesource from "@/component/molecules/Homesource";
-=======
-import Contactus from "@/component/templates/contactUs/Contactus";
-
->>>>>>> efb3479bbcb1a90b21208b7fc26233c9a13a143d
+import Homesource from "@/component/molecules/Homesource"; // Assuming you want to keep this import
 
 function Home() {
   return (
-    <div className=" " >
-<<<<<<< HEAD
-      {/* <Homesrc/> */}
-      <Homesource/>
-=======
-      <Contactus />
->>>>>>> efb3479bbcb1a90b21208b7fc26233c9a13a143d
+    <div className="">
+      <Homesource />
     </div>
   );
 }
