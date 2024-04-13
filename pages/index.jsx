eb478ed42c0cb6f9ce05d66c0d1347f4f5b20fed
genroar <genroar7@gmail.com>
2024-04-13 15:44:59 +0500

    cloning into new laptop

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,13 @@
-import React from "react";
-import "../../strapup/app/globals.css";
-import Blog from "@/component/templates/Blog";
-import Header from "@/component/templates/Header";
-import CardSubmit from "@/component/organisms/CardSubmit";
 import Home from "@/component/templates/Home";
+import "../../strapup/app/globals.css";
 
-<<<<<<< HEAD
 function index() {
   return (
     <div className="">
-      <Home />
+      <div className="main">
+        <h1>i am the devel of my world </h1>
+      </div>
     </div>
   );
-=======
-import React from 'react'
-import Sell from './sell'
-import Watch from '@/component/molecules/Watch'
-import CardSubmit from '@/component/organisms/CardSubmit'
-import Submitment from '@/component/organisms/Submitment'
-import RolexDay from '@/component/organisms/RolexDay'
-
-function index() {
-  return (
-    <div>
-        {/* <Header/>
-        <News/>
-        <Footer/> */}
-        {/* <Createlisting/> */}
-        {/* <Createorgan/> */}
-        {/* <Submitment/> */}
-        {/* <Homesource/> */}
-        {/* <Source2/> */}
-        {/* <Sell/>         */}
-        {/* <Account/> */}
-        {/* <CardSubmit/> */}
-        <RolexDay/>
-    </div>  
-  )
->>>>>>> 07a3dfee1c943b5f7100230003bfe98beb79294a
-}
+  }
 export default index;
