4bb7a5e26ca1071fb11eb5bcb0551981a65283f4
Oracle 🔥 <152638304+Ali-2544@users.noreply.github.com>
2024-04-26 03:47:27 +0500

    some conflicts are comming and that were solved now

--- component/organisms/BuyHome.jsx
+++ component/organisms/BuyHome.jsx
@@ -10,13 +10,13 @@ function BuyHome() {
         <div className='px-[15px]'>
             <Listingsearch />
           </div>
-          <div className="flex flex-col gap-[30px]">
+          <div className="flex flex-col px-[20px] gap-[30px]">
             <div className="flex justify-between pt-[20px] items:center" >
               <div><Heading level="1">Recent listings</Heading></div>
               <Button variant='oval' >View All Listing</Button>
             </div>
             <div>
-              <div className="flex gap-[30px] items-center flex-wrap" >
+              <div className="flex gap-[30px] justify-between items-center flex-wrap" >
               <Product  productClass='product_cols'/>
               <Product  productClass='product_cols'/>
               <Product  productClass='product_cols'/>
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '@/component/templates/Home';
+import Account from './account';
+// Import other components as needed
+
+function index() {
+  return (
+    <div>
+      <Home/>
+      {/* <Account /> */}
+      {/* Add other components here */}
+    </div>
+  );
+}
+
+export default index;
+  
